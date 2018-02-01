7c3ca5696935af5347d44133c69bfcf02bde5c1a
MengXiaoyang <mxywo@qq.com>
2018-02-01 09:48:35 +0800

    Initial commit

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.taotao.service.service;
+
+
+import com.taotao.pojo.TbItem;
+import pojo.EUDadaGridResult;
+import utils.TaotaoResult;
+
+public interface ItemService {
+
+	TbItem getItemById(long itemId);
+	EUDadaGridResult getItemList(int page,int rows);
+	TaotaoResult addItem(TbItem tbItem,String desc) throws Exception;
+	TaotaoResult addItemDesc(Long id , String desc);
+}
